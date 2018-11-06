725e8bdbda04980ad6a88f84c9aa1f9411fc55b8
Viktor.Punko <Viktor.Punko@nexign-systems.com>
2018-11-06 12:37:28 +0300

    update config

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -253,8 +253,8 @@ module.exports = {
               ]),
               // @remove-on-eject-end
               plugins: [
-                [require.resolve('@babel/plugin-proposal-class-properties'), { "loose" : true }],
                 [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
+                [require.resolve('@babel/plugin-proposal-class-properties'), { "loose" : true }],
                 [
                   require.resolve('babel-plugin-named-asset-import'),
                   {
